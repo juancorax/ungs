28f1123f9e98c841d2d9de2c0a922f1e05aac202
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-28 00:53:54 -0300

    fix: exercise 2 from 'condicionales'

--- introduccion-a-la-programacion/condicionales/ejercicio_02.py
+++ introduccion-a-la-programacion/condicionales/ejercicio_02.py
@@ -1,2 +1,10 @@
 # Hacer un programa que solicite dos números e indique el
 # signo de la multiplicación sin efectuarla.
+
+numeroSolicitado1 = int(input("Ingrese un número:\n"))
+numeroSolicitado2 = int(input("Ingrese otro número:\n"))
+
+if numeroSolicitado1 * numeroSolicitado2 >= 0:
+    print("El signo de la multiplicación es positivo")
+else:
+    print("El signo de la multiplicación es negativo")
